a45bb88c77f1df52deea287a8c939fbbe52d088a
SVYSHE <rphp@web.de>
2021-04-14 10:55:55 +0200

    Made month constants private

--- src/main/java/com/github/svyshe/date/DateTime.java
+++ src/main/java/com/github/svyshe/date/DateTime.java
@@ -8,18 +8,8 @@ import java.io.Serializable;
  */
 public class DateTime implements Serializable, Comparable<DateTime> {
 
-    public static final int JANUARY = 1;
-    public static final int FEBRUARY = 2;
-    public static final int MARCH = 3;
-    public static final int APRIL = 4;
-    public static final int MAY = 5;
-    public static final int JUNE = 6;
-    public static final int JULY = 7;
-    public static final int AUGUST = 8;
-    public static final int SEPTEMBER = 9;
-    public static final int OCTOBER = 10;
-    public static final int NOVEMBER = 11;
-    public static final int DECEMBER = 12;
+    private static final int JANUARY = 1;
+    private static final int DECEMBER = 12;
 
     private int year;
     private int month;
