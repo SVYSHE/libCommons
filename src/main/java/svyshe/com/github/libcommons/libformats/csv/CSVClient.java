64360be06e72f51cb50f05d36e5bed3d2186070f
SVYSHE <rphp@web.de>
2021-09-25 16:36:11 +0200

    JavaDoc and reader.close() optimization.
    - Added JavaDoc
    - Made the `close()` method check, whether the reader is `null`

--- src/main/java/svyshe/com/github/libcommons/libformats/csv/CSVClient.java
+++ src/main/java/svyshe/com/github/libcommons/libformats/csv/CSVClient.java
@@ -12,29 +12,62 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * This class is supposed to offer a way to handle CSV files.
+ * In difference to other existing CSV libraries, CSVClient will offer you options to read a file line by line or even
+ * the whole file at once which will be less memory efficient in further processing steps.
+ *
+ * @author https://www.github.com/SVYSHE
+ * @version 1.0
+ */
 public class CSVClient implements AutoCloseable {
 
     private File file;
     private CSVDelimiter delimiter;
     private final Scanner reader;
 
+    /**
+     * Getter for the CSV file.
+     *
+     * @return The CSV file.
+     */
     public File getFile() {
         return file;
     }
 
+    /**
+     * The setter for the CSV file.
+     *
+     * @param file The file to set as CSV file.
+     */
     public void setFile(File file) {
         this.file = file;
     }
 
+    /**
+     * The getter for the delimiter in read mode.
+     *
+     * @return The delimiter in read mode.
+     */
     public CSVDelimiter getDelimiter() {
         return delimiter;
     }
 
+    /**
+     * The setter for the delimiter in read mode.
+     *
+     * @param delimiter The delimiter to set for read mode.
+     */
     public void setDelimiter(CSVDelimiter delimiter) {
         this.delimiter = delimiter;
     }
 
-
+    /**
+     * Constructs a new CSV client and initializes it with the specified CSV file.
+     *
+     * @param file The CSV file that the client should operate on.
+     * @throws CSVClientException If the specified file can't be found.
+     */
     public CSVClient(File file) throws CSVClientException {
         this.file = file;
         this.delimiter = CSVDelimiter.COMMA;
@@ -45,6 +78,11 @@ public class CSVClient implements AutoCloseable {
         }
     }
 
+    /**
+     * Reads the whole CSV file and returns it as a list of string arrays. The delimiter has to be already specified.
+     *
+     * @return A list of string arrays representing the whole file.
+     */
     public List<String[]> readAll() {
         List<String[]> content = new LinkedList<>();
         while (hasNextLine()) {
@@ -56,6 +94,11 @@ public class CSVClient implements AutoCloseable {
         return content;
     }
 
+    /**
+     * Reads a single line from the CSV file starting from the top.
+     *
+     * @return A string array containing the fields.
+     */
     public String[] readLine() {
         String line = reader.nextLine();
         if (StringUtils.isEmpty(line)) {
@@ -70,10 +113,23 @@ public class CSVClient implements AutoCloseable {
         return StringUtils.splitByWholeSeparator(line, String.valueOf(this.delimiter.character));
     }
 
+    /**
+     * Checks whether the file-cursors next position will be a line.
+     *
+     * @return True if there is a next line, false if not.
+     */
     public boolean hasNextLine() {
         return reader.hasNextLine();
     }
 
+    /**
+     * Appends a line to the set file.
+     *
+     * @param line      The contents to write to the file.
+     * @param charset   The charset used to write with.
+     * @param delimiter The delimiter to separate the single fields.
+     * @throws CSVClientException In case the writer isn't able to write to the specified file.
+     */
     public void appendLine(String[] line, Charset charset, CSVDelimiter delimiter) throws CSVClientException {
         String outputString = String.join(delimiter.character, line) + "\r\n";
         try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(String.valueOf(file)), charset, StandardOpenOption.APPEND)) {
@@ -83,21 +139,46 @@ public class CSVClient implements AutoCloseable {
         }
     }
 
+    /**
+     * Writes multiple lines at once to a file.
+     *
+     * @param lines     The lines represented as a list of string arrays.
+     * @param charset   The charset used to write with.
+     * @param delimiter The delimiter to separate the single fields.
+     * @throws CSVClientException In case the writer isn't able to write to the specified file.
+     */
     public void writeAll(List<String[]> lines, final Charset charset, final CSVDelimiter delimiter) throws CSVClientException {
         for (String[] line : lines) {
             appendLine(line, charset, delimiter);
         }
     }
 
+    /**
+     * Closes the reader.
+     */
     @Override
     public void close() {
-        this.reader.close();
+        if (this.reader != null) {
+            this.reader.close();
+        }
     }
 
+    /**
+     * Checks whether the line (represented as single string) contains an odd number of double quotes.
+     *
+     * @param line The line as string to check.
+     * @return true if it has an odd count of double quotes, false if not.
+     */
     private boolean hasOddCountOfDoubleQuotes(String line) {
         return StringUtils.countMatches(line, "\"") % 2 != 0;
     }
 
+    /**
+     * Escapes double quotes in the given line (represented as single string).
+     *
+     * @param line The line as string to escape the double quotes from.
+     * @return The escaped line.
+     */
     private String escapeDoubleQuotes(String line) {
         line = line.replace("\"", "");
         return line;
